0acf02e775c433496a50d7acd91f435826372622
barbatrukko <barbatrukko@gmail.com>
2022-06-17 21:58:47 +0200

    secons commit

--- aws-instance-first-script/aws-instance-example.tf
+++ aws-instance-first-script/aws-instance-example.tf
@@ -6,6 +6,12 @@ resource "aws_instance" "web1" {
     tags = {
     Name = "myFirstWebServer"
   }
+
+    provisioner "remote-exec" {
+    inline = [
+      "cloud-init status --wait"
+    ]
+  }
     provisioner "file" {
     source      = "web/index.html"
     destination = "/opt/index.html"
@@ -14,4 +20,11 @@ resource "aws_instance" "web1" {
     source      = "web/iei.jpg"
     destination = "/opt/iei.jpg"
   }
+
+    connection {
+    user        = "ec2-user"
+    private_key = "${TF_VAR_keypem}"
+      host = "${aws_instance.web-server.public_ip}"
+  }
+
  }
