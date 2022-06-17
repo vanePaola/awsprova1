6804d189165b834c9d14a437a9abb1f514647d29
barbatrukko <barbatrukko@gmail.com>
2022-06-17 18:31:05 +0200

    Update variables.tf

--- aws-instance-first-script/variables.tf
+++ aws-instance-first-script/variables.tf
@@ -6,8 +6,8 @@ variable "ami_id" {
   type = map
 
   default = {
-    eu-west-1    = "ami-035b3c7efe6d061d5"
-    eu-west-2    = "ami-132b3c7efe6sdfdsfd"
-    eu-south-1   = "ami-9787h5h6nsn"
+    eu-west-1    = "ami-d12741a2"
+    eu-west-2    = "ami-d12741a2"
+    eu-south-1   = "ami-d12741a2"
   }
 }
