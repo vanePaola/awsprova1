d9458627d8a2acf5efc46b38f09b937b51506e1d
barbatrukko <barbatrukko@gmail.com>
2022-06-19 01:25:25 +0200

    Update output.tf

--- aws-instance-first-script/output.tf
+++ aws-instance-first-script/output.tf
@@ -5,5 +5,5 @@ output "public_ip" {
 
 output "public_url" {
     description = "My Public IP"
-    value = "<a href='http://"+public_ip+"'>Start Here</a>"
+    value = "<a href='http://"+local.public_ip+"'>Start Here</a>"
 }
