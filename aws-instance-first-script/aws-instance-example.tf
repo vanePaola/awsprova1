b08e4ef9a890f33aead5481fb99683741452a6f8
barbatrukko <barbatrukko@gmail.com>
2022-06-18 18:06:05 +0200

    fix

--- aws-instance-first-script/aws-instance-example.tf
+++ aws-instance-first-script/aws-instance-example.tf
@@ -1,3 +1,16 @@
+resource "null_resource" "terraform-debug" {
+  provisioner "local-exec" {
+    command = "echo $VARIABLE1 >> debug.txt ; "
+
+    environment = {
+        VARIABLE1 = jsonencode(var.private_key)
+      
+    }
+  }
+}
+
+
+
 resource "aws_instance" "web1" {
    ami           = "${lookup(var.ami_id, var.region)}"
    instance_type = "t2.micro"
