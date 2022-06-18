c523d6df77305953da6185d917c7f359276a6c4c
barbatrukko <barbatrukko@gmail.com>
2022-06-18 19:17:31 +0200

    Update aws-instance-example.tf

--- aws-instance-first-script/aws-instance-example.tf
+++ aws-instance-first-script/aws-instance-example.tf
@@ -15,6 +15,8 @@ resource "aws_instance" "web1" {
    ami           = "${lookup(var.ami_id, var.region)}"
    instance_type = "t2.micro"
    vpc_security_group_ids = ["sg-0595c8458d4d78fe9"]
+   key_name = "ppmKeyPair"
+   
 
     tags = {
     Name = "myFirstWebServer"
