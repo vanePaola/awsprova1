77769606ed09d2e1ae475448c8d17d78505797b3
barbatrukko <barbatrukko@gmail.com>
2022-06-19 01:20:36 +0200

    Update output.tf

--- aws-instance-first-script/output.tf
+++ aws-instance-first-script/output.tf
@@ -2,3 +2,8 @@ output "public_ip" {
     description = "My Public IP"
     value = try(aws_instance.web1.public_ip,"")
 }
+
+output "public_url" {
+    description = "My Public IP"
+    value = "<a href='http://"+public_ip+"'>Start Here</a>"
+}
