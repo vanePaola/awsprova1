e724770d8b2760e9432464376db2849f014c4572
barbatrukko <barbatrukko@gmail.com>
2022-06-19 15:36:19 +0200

    Update aws-instance-example.tf

--- aws-instance-first-script/aws-instance-example.tf
+++ aws-instance-first-script/aws-instance-example.tf
@@ -83,6 +83,13 @@ depends_on = [ aws_instance.web1 ]
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
+  
+  ingress {
+    from_port   = 8080  
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 
   egress {
     from_port       = 0
