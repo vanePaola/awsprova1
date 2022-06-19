3eff7dae12a9b50fddfbb8b9699118f9b6b1baea
barbatrukko <barbatrukko@gmail.com>
2022-06-19 13:23:37 +0200

    ansible

--- aws-instance-first-script/script.sh
+++ aws-instance-first-script/script.sh
@@ -1,5 +1,5 @@
-sudo amazon-linux-extras install -y java-openjdk11
-java -version
+#sudo amazon-linux-extras install -y java-openjdk11
+#java -version
 sudo yum update -y
 sudo yum install -y httpd.x86_64
 sudo systemctl start httpd.service
