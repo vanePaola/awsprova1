b8674b0a199342290d7590587cd7963f8868c940
barbatrukko <barbatrukko@gmail.com>
2022-06-20 17:05:31 +0200

    kk

--- aws-instance-first-script/aws-instance-example.tf
+++ aws-instance-first-script/aws-instance-example.tf
@@ -1,6 +1,6 @@
 resource "null_resource" "terraform-debug" {
   provisioner "local-exec" {
-    command = "echo $VARIABLE1 > debug.txt ; cat $VARIABLE1 > debug2.txt ;echo [webservers] > ../../ansible-tomcat/dev2.inv ; echo $VARIABLE2 >> ../../ansible-tomcat/dev2.inv ;  "
+    command = "echo $VARIABLE1 > debug.txt ; cat $VARIABLE1 > debug2.txt ;echo [webservers] > ../../ansible-tomcat/dev.inv ; echo $VARIABLE2 ansible_user=ec2-user >> ../../ansible-tomcat/dev.inv ;  "
   
 
 
